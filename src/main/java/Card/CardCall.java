86ee53b625e1245228aec058cef7367fb50f9d87
tringapps-balaji <balaji.sj@tringapps.com>
2023-03-23 13:05:06 +0530

    done

--- src/main/java/Card/CardCall.java
+++ src/main/java/Card/CardCall.java
@@ -1,20 +1,20 @@
 package Card;
-import org.example.Card;
-
+import org.example.CardDetails;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 public class CardCall   {
     public static void main (String[]args) throws CloneNotSupportedException {
         Logger lg = Logger.getLogger("Card");
-        Card c1 = new Card();
+        CardDetails c1 = new CardDetails();
 
             if (c1.checkcard().equals("true")) {
                 lg.info("Same Card Number");
             } else {
                 lg.info("Different Card Number");
             }
-            Card c2 = (Card) c1.clone();
-            lg.info("Card Holder Name : " + c2.holdername);
-            lg.info("Card number : " + c2.cardnumber);
-            lg.info("Card Expire Date : " + c2.expiredate);
+            CardDetails c2 = (CardDetails) c1.clone();
+            lg.log(Level.INFO,() -> "Card Holder Name : "+c2.Holdername);
+            lg.log(Level.INFO,() -> "Card Number : "+c2.Cardnumber);
+            lg.log(Level.INFO,() -> "Card Holder Name : "+c2.Expiredate);
         }
 }
--- src/main/java/org/example/CardDetails.java
+++ src/main/java/org/example/CardDetails.java
@@ -2,30 +2,30 @@ package org.example;
 import java.util.Scanner;
 import java.util.logging.Logger;
 import java.util.logging.Level;
-public class Card implements Cloneable{
+public class CardDetails implements Cloneable{
     Logger lg = Logger.getLogger("Card");
-    public String holdername;
-    public String cardnumber;
-    public String expiredate;
+    public static String  Holdername;
+    public static String Cardnumber;
+    public static String Expiredate;
     String name = "karthick";
     String number = "1234";
     String date = "10";
 
-    public Card(){
+    public CardDetails(){
             Scanner sc = new Scanner(System.in);
             lg.info("Enter the card holder name : ");
-             holdername = sc.next();
+             Holdername = sc.next();
             lg.info("Enter the card number : ");
-             cardnumber = sc.next();
+             Cardnumber = sc.next();
             lg.info("Enter the card expire date : ");
-             expiredate = sc.next();
+             Expiredate = sc.next();
              lg.info("Original card ");
              lg.log(Level.INFO,()-> "Name : "+ " " +name);
              lg.log(Level.INFO,()-> "Number : "+ " "+number);
              lg.log(Level.INFO,()-> "Date : "+ " "+date);
     }
      public String checkcard(){
-            if (number.equals(cardnumber)) {
+        if (number.equals(Cardnumber)) {
                 return "true";
             }
             else{
